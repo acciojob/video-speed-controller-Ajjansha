8782e97415308f8875480fbbc623a8a69483069f
Ajjansha <141652880+Ajjansha@users.noreply.github.com>
2026-02-03 23:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const inputs = document.querySelectorAll('.controls input');
+const speed = document.querySelector('.speed');
+const bar = document.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function handleMove(e) {
+  // Get position inside the speed container
+  const y = e.offsetY;
+  const percent = y / speed.offsetHeight;
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+  // Set min and max playback speed
+  const min = 0.4;
+  const max = 4;
+
+  // Calculate playback rate
+  const playbackRate = percent * (max - min) + min;
+
+  // Update bar height & text
+  bar.style.height = `${Math.round(percent * 100)}%`;
+  bar.textContent = playbackRate.toFixed(1) + '×';
+
+  // Apply speed to video
+  video.playbackRate = playbackRate;
+}
+
+// Mouse move inside speed bar
+speed.addEventListener('mousemove', handleMove);
